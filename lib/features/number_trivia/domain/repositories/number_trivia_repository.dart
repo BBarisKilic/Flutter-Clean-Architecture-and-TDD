9c8190cf12eaccd18af1dafda1b29a37e8625c7b
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2021-10-28 21:55:12 +0200

    Created NumberTriviaRepository abstract class.

---
+++ lib/features/number_trivia/domain/repositories/number_trivia_repository.dart
@@ -0,0 +1,8 @@
+import 'package:clean_architecture_and_tdd/core/error/failure.dart';
+import 'package:clean_architecture_and_tdd/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class NumberTriviaRepository {
+  Future<Either<Failure, NumberTrivia>> getConcereteNumberTrivia(int number);
+  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia();
+}
