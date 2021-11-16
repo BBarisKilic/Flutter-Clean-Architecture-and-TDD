db40827509e714d60b7fa9b6a5e3aa93377e1585
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2021-11-16 17:49:33 +0100

    Created RandomNumber test.

---
+++ test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
@@ -0,0 +1,35 @@
+import 'package:clean_architecture_and_tdd/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:clean_architecture_and_tdd/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:clean_architecture_and_tdd/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class MockNumberTriviaRepository extends Mock
+    implements NumberTriviaRepository {}
+
+void main() {
+  late GetRandomNumberTrivia usecase;
+  late MockNumberTriviaRepository mockNumberTriviaRepository;
+
+  setUp(() {
+    mockNumberTriviaRepository = MockNumberTriviaRepository();
+    usecase = GetRandomNumberTrivia(mockNumberTriviaRepository);
+  });
+
+  const tNumberTrivia = NumberTrivia(number: 1, text: 'test');
+
+  test(
+    'should get trivia from the repository',
+    () async {
+      when(mockNumberTriviaRepository.getRandomNumberTrivia())
+          .thenAnswer((_) async => const Right(tNumberTrivia));
+
+      final result = await usecase();
+
+      expect(result, const Right(tNumberTrivia));
+      verify(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber));
+      verifyNoMoreInteractions(mockNumberTriviaRepository);
+    },
+  );
+}
